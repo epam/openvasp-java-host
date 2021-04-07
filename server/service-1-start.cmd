ab021d371b8cd9bd0907acc08fceed186c754d4c
Val Kvasov <valentynkvasov@gmail.com>
2021-04-07 11:25:27 +0300

    Introduce dockerfile for beneficiary\originartor host

--- server/service-1-start.cmd
+++
@@ -1,2 +0,0 @@
-
-java -Dspring.profiles.active=db1,caffeine -jar target\host-0.0.2.jar --openvasp.host.configNr=1 --server.port=8080
--- server/service-2-start.cmd
+++
@@ -1,2 +0,0 @@
-
-java -Dspring.profiles.active=db2,caffeine -jar target\host-0.0.2.jar --openvasp.host.configNr=2 --server.port=8081
